9a1338e6b1a944a354115e6a78ddeb11ea511f00
dmitrymitrakhovich <dmitrymitrakhovich@gmail.com>
2022-05-10 23:04:03 +0400

    refactoring main component

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,29 +1,19 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Movies from "../components/Movies";
 import { Prealoader } from "../components/Prealoader";
 import Search from "../components/Search";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-export default class Main extends Component {
-  state = {
-    movies: [],
-    loading: false,
-  };
+export default function Main() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  componentDidMount() {
-    this.setState({ loading: true });
-    try {
-      fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
-        .then((res) => res.json())
-        .then((data) => this.setState({ movies: data.Search, loading: false }));
-    } catch (error) {
-      console.error(error);
-      this.setState({ loading: false });
-    }
-  }
+  useEffect(() => {
+    searchMovie();
+  }, [movies]);
 
-  searchMovie = (str, type = "all") => {
-    this.setState({ loading: true });
+  const searchMovie = (str = "matrix", type = "all") => {
+    setLoading(true);
     try {
       fetch(
         `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
@@ -31,20 +21,20 @@ export default class Main extends Component {
         }`
       )
         .then((res) => res.json())
-        .then((data) => this.setState({ movies: data.Search, loading: false }));
+        .then((data) => {
+          setMovies(data.Search);
+          setLoading(false);
+        });
     } catch (error) {
       console.error(error);
-      this.setState({ loading: false });
+      setLoading(false);
     }
   };
 
-  render() {
-    const { movies, loading } = this.state;
-    return (
-      <main className='container content'>
-        <Search searchMovie={this.searchMovie} />
-        {loading ? <Prealoader /> : <Movies movies={movies} />}
-      </main>
-    );
-  }
+  return (
+    <main className='container content'>
+      <Search searchMovie={searchMovie} />
+      {loading ? <Prealoader /> : <Movies movies={movies} />}
+    </main>
+  );
 }
